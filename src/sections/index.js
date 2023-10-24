376ff8f80320a98047162ae0cd81e9a37e197c7f
Sadam Endris <sadamendris48@gmail.com>
2023-10-24 11:20:21 +0300

    first commit

---
+++ src/sections/index.js
@@ -0,0 +1,18 @@
+import Hero from "./Hero";
+import Services from "./Services";
+import SuperQuality from "./SuperQuality";
+import Subscribe from "./Subscribe";
+import Footer from "./Footer";
+import PopularProducts from "./PopularProducts";
+import SpecialOffer from "./SpecialOffer";
+import CustomerReviews from "./CustomerReviews";
+export {
+  Hero,
+  Services,
+  SuperQuality,
+  Subscribe,
+  Footer,
+  PopularProducts,
+  SpecialOffer,
+  CustomerReviews,
+};
